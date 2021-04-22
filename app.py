a2fdcc86eeced2ddcf1753fd7242cb43ee411740
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-04-22 17:51:14 -0400

    Module Complete

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_pymongo import PyMongo
+from flask_pymongo import pymongo
 import scraping
 
 #Set up flask
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
+app.config["MONGO_URL"] = "mongodb://localhost:27017/mars_app"
+mongo = pymongo(app)
 
 # Define route for HTML page
 @app.route("/")
